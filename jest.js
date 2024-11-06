0ec1b007c61ebbd606e2b51c47e92c61fb500d25
Diego Mendoza Pinto <dgvip1998@gmail.com>
2024-11-06 18:51:09 -0500

    fix: remove deprecated rules from jest config (#9)

--- jest.js
+++ jest.js
@@ -65,7 +65,6 @@ module.exports = {
         "jest/no-if": "error",
         "jest/no-interpolation-in-snapshots": "error",
         "jest/no-jasmine-globals": "off",
-        "jest/no-jest-import": "error",
         "jest/no-large-snapshots": ["warn", { maxSize: 300 }],
         "jest/no-mocks-import": "error",
         "jest/no-restricted-matchers": "off",
